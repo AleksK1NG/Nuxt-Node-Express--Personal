83281edd3d0034e39202ea7403d375d46107ae46
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-05 12:37:23 +0300

    add Hero component

---
+++ components/coursePage/coursecard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'coursecard'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <section class="hero is-black is-medium">
-      <div class="hero-body">
-        <div
-          class="hero-img"
-          :style="{
-            background: `url(https://images.unsplash.com/photo-1510519138101-570d1dca3d66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1631&q=80) no-repeat center center`
-          }"
-        ></div>
-        <div class="container">
-          <h1 class="title">
-            Super Amazing Promo
-          </h1>
-          <h2 class="subtitle">
-            Super Amazing Promo Subtitle
-          </h2>
-          <a target="_" :href="'#'" class="button is-danger">Learn More!</a>
-        </div>
-      </div>
-    </section>
+    <Hero />
     <section class="section">
       <div class="container">
         <h1 class="title">Featured Courses</h1>
@@ -29,7 +11,7 @@
               <div class="card-image">
                 <figure class="image is-4by2">
                   <img
-                    src="https://images.unsplash.com/photo-1547658718-1cdaa0852790?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    src="https://images.unsplash.com/photo-1544980919-e17526d4ed0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
                     alt="Placeholder image"
                   />
                 </figure>
@@ -92,8 +74,9 @@
 </template>
 
 <script>
+import Hero from '../components/shared/Hero'
 export default {
-  components: {}
+  components: { Hero }
 }
 </script>
 
@@ -116,41 +99,7 @@ export default {
   }
 }
 // card item end
-// hero
-.hero-body {
-  position: relative;
-}
-.hero-img {
-  opacity: 0.8;
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  top: 0;
-  left: 0;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-.user-avatar {
-  display: inline-block;
-}
-.is-black {
-  background-color: black;
-}
-.title {
-  font-weight: bold;
-  font-size: 45px;
-}
-.subtitle {
-  /*font-weight: bold;*/
-  font-size: 25px;
-}
-.author-name {
-  font-size: 20px;
-  font-weight: bold;
-}
-// hero
+
 // Home page
 .links {
   padding-top: 15px;
