98bbc3b124a3cbeda49ab203a2b724171dfe6755
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-06 17:16:54 +0300

    add instructor page

---
+++ pages/instructor/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="instructor-page">
+    <div class="centered">
+      <div class="columns">
+        <!-- Go to /instructor/courses -->
+        <div class="box" @click="() => {}">
+          <div>
+            Courses
+          </div>
+        </div>
+        <!-- Go to /instructor/blogs -->
+        <div class="box" @click="() => {}">
+          <div>
+            Blogs
+          </div>
+        </div>
+        <!-- Go to /instructor/heroes -->
+        <div class="box" @click="() => {}">
+          <div>
+            Heroes
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'instructor',
+    middleware: 'admin'
+  }
+</script>
+
+<style scoped></style>
