b357889fc5d043463fdb21598738c21708a0f665
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-06 18:19:16 +0300

    add middlewares redirect routes

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,6 @@ export default function({ store, redirect }) {
   const isAdmin = store.getters['auth/isAdminGetter']
 
   if (!isAdmin) {
-    return redirect('/')
+    return redirect('/notAuthorized')
   }
 }
