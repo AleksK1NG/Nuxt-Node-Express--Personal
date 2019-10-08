c38556e0fbb53a9e103a75ee1f9375e68536b263
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-08 13:49:52 +0300

    add category select options bind data

--- components/insctructor/CreateCourseStepTwo.vue
+++ components/insctructor/CreateCourseStepTwo.vue
@@ -3,22 +3,20 @@
     <div class="course-create-headerText">
       What category best fits the knowledge you'll share?
     </div>
-    <h2 class="course-create-subtitle">
-      If you're not sure about the right category, you can change it later.
-    </h2>
+    <h2 class="course-create-subtitle">If you're not sure about the right category, you can change it later.</h2>
     <form class="course-create-form">
       <div class="course-create-form-group">
         <div class="course-create-form-field">
           <div class="select is-large">
-            <select>
+            <select v-model="form.category" @blur="$v.form.category.$touch()" @change="emitFormData">
               <option value="default">Select Category</option>
-              <option>
-                Mobile Development
-              </option>
-              <option>
-                Web Development
+              <option v-for="category in categories" :key="category._id" :value="category._id">
+                {{ category.name }}
               </option>
             </select>
+          </div>
+          <div v-if="$v.form.category.$dirty && !isValid" class="form-error">
+            <span class="help is-danger">Category is required!</span>
           </div>
         </div>
       </div>
@@ -26,7 +24,40 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'CreateCourseStepTwo'
+import { required } from 'vuelidate/lib/validators'
+export default {
+  name: 'CreateCourseStepTwo',
+  data: () => ({
+    form: {
+      category: 'default'
+    }
+  }),
+  validations: {
+    form: {
+      category: {
+        required
+      }
+    }
+  },
+  computed: {
+    isValid() {
+      return !this.$v.$invalid && this.form.category !== 'default'
+    },
+    categories() {
+      return this.$store.state.category.categories
+    }
+  },
+  methods: {
+    emitFormData() {
+      this.$v.form.$touch()
+      this.$emit('updateStep', { data: this.form, isValid: this.isValid })
+    }
   }
+}
 </script>
+
+<style scoped>
+.help.is-danger {
+  text-align: left;
+}
+</style>
