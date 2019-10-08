ce3190a3f5bc3cda661314670a006c79525156e9
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-08 11:10:30 +0300

    add create course steps components

---
+++ pages/instructor/course/create.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="full-page-takeover-window">
+    <div class="full-page-takeover-page">
+      <InstructorHeader title="Step 1 of 2" exitLink="#" />
+      <div class="full-page-takeover-header-bottom-progress">
+        <div :style="{ width: '50%' }" class="full-page-takeover-header-bottom-progress-highlight"></div>
+      </div>
+      <div class="course-create full-page-takeover-container">
+        <div class="container">
+          <!-- STEP 1 of FORM  -->
+          <CreateCourseStepOne />
+          <!-- STEP 1 END-->
+          <!-- STEP 2 of FORM -->
+          <CreateCourseStepTwo />
+          <!-- STEP 2 END -->
+        </div>
+        <div class="full-page-footer-row">
+          <div class="container">
+            <div class="full-page-footer-col">
+              <div>
+                <a @click.prevent="() => {}" class="button is-large">Previous</a>
+              </div>
+              <!-- <div v-else class="empty-container">
+              </div> -->
+            </div>
+            <div class="full-page-footer-col">
+              <div>
+                <button @click.prevent="() => {}" class="button is-large float-right">
+                  Continue
+                </button>
+                <!-- <button
+                  @click="() => {}"
+                  class="button is-success is-large float-right">
+                  Confirm
+                </button> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CreateCourseStepOne from '../../../components/insctructor/CreateCourseStepOne'
+import CreateCourseStepTwo from '../../../components/insctructor/CreateCourseStepTwo'
+import InstructorHeader from '../../../components/shared/Header'
+export default {
+  name: 'createCourse',
+  components: { InstructorHeader, CreateCourseStepTwo, CreateCourseStepOne }
+}
+</script>
+
+<style lang="scss">
+.float-right {
+  float: right;
+}
+.empty-container {
+  width: 1px;
+  height: 1px;
+}
+.course-create {
+  &-wrapper {
+    margin-top: 60px;
+    text-align: center;
+  }
+  &-headerText {
+    font-weight: 500;
+    line-height: 1.1;
+    margin-top: 21px;
+    margin-bottom: 10.5px;
+    font-size: 36px;
+    font-weight: 300;
+  }
+  &-subtitle {
+    font-size: 24px;
+    font-weight: 300;
+    margin-top: 21px;
+    margin-bottom: 10.5px;
+  }
+  &-form {
+    margin-top: 60px;
+    &-group {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    &-field {
+      min-width: 552px;
+    }
+  }
+}
+</style>
