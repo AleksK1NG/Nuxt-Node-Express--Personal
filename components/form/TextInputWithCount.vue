516f279e8d8136d162e8f8619b4f7483373fcac1
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-09 12:34:44 +0300

    add TextInputWithCount component

---
+++ components/form/TextInputWithCount.vue
@@ -0,0 +1,18 @@
+<template>
+  <input :maxLength="maxLength" type="text" placeholder="e.g. Amazing Course in Flutter!" class="input is-large" />
+</template>
+
+<script>
+export default {
+  name: 'TextInputWithCount',
+  props: {
+    maxLength: {
+      type: Number,
+      default: 50,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped></style>
