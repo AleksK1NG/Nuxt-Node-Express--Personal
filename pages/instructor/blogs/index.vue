1e904678092dd1fcd7dc65e38747c0c7b5cb8105
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-14 11:03:30 +0300

    add instructor blogs page map store data

--- pages/instructor/blogs/index.vue
+++ pages/instructor/blogs/index.vue
@@ -12,9 +12,11 @@
           </div>
           <div class="tabs">
             <ul>
+              <!-- set here active tab -->
               <li @click="activeTab = 0">
                 <a :class="{ 'is-active': activeTab === 0 }">Drafts</a>
               </li>
+              <!-- set here active tab -->
               <li @click="activeTab = 1">
                 <a :class="{ 'is-active': activeTab === 1 }">Published</a>
               </li>
@@ -24,58 +26,40 @@
             <!-- Draft Blogs -->
             <!-- check for active tab -->
             <template v-if="activeTab === 0">
-              <div>
-                <div class="blog-card">
-                  <h2>Some Title</h2>
+              <div v-if="drafts && drafts.length > 0">
+                <!-- make iteration here for drafts -->
+                <div v-for="dBlog in drafts" :key="dBlog._id" class="blog-card">
+                  <h2>{{ dBlog.title }}</h2>
                   <div class="blog-card-footer">
-                    <span>
-                      Last Edited 17th December, 2018
-                    </span>
-                    <!-- Dropdown with menu here -->
-                  </div>
-                </div>
-                <div class="blog-card">
-                  <h2>Some Title</h2>
-                  <div class="blog-card-footer">
-                    <span>
-                      Last Edited 17th December, 2018
-                    </span>
+                    <span> Last Edited {{ dBlog.updatedAt | formatDate('LLLL') }} </span>
                     <!-- Dropdown with menu here -->
                   </div>
                 </div>
               </div>
               <!-- In case of no drafts blogs  -->
-              <!-- <div class="blog-error">
+              <div v-else class="blog-error">
                 No Drafts :(
-              </div> -->
+              </div>
             </template>
             <!-- Published Blogs -->
             <!-- check for active tab -->
             <template v-if="activeTab === 1">
-              <div>
-                <div class="blog-card">
-                  <h2>Published Blog</h2>
+              <div v-if="published && published.length > 0">
+                <!-- make iteration here for published -->
+                <div v-for="pBlog in published" :key="pBlog._id" class="blog-card">
+                  <!-- title -->
+                  <h2>{{ pBlog.title }}</h2>
                   <div class="blog-card-footer">
-                    <span>
-                      Last Edited 17th December, 2018
-                    </span>
-                    <!-- Dropdown with menu here -->
-                  </div>
-                </div>
-                <div class="blog-card">
-                  <h2>Published Blog</h2>
-                  <div class="blog-card-footer">
-                    <span>
-                      Last Edited 17th December, 2018
-                    </span>
+                    <!-- updatedAt -->
+                    <span> Last Edited {{ pBlog.updatedAt | formatDate('LLLL') }} </span>
                     <!-- Dropdown with menu here -->
                   </div>
                 </div>
               </div>
               <!-- In case of no drafts blogs  -->
-              <!-- <div class="blog-error">
-                No Drafts :(
-              </div> -->
+              <div v-else class="blog-error">
+                No Published Blogs :(
+              </div>
             </template>
           </div>
         </div>
@@ -85,6 +69,7 @@
 </template>
 <script>
 import Header from '~/components/shared/Header'
+import { mapState } from 'vuex'
 export default {
   layout: 'instructor',
   components: { Header },
@@ -92,6 +77,12 @@ export default {
   data: () => ({
     activeTab: 0
   }),
+  computed: {
+    ...mapState({
+      published: ({ instructor }) => instructor.blog.userBlogs.published,
+      drafts: ({ instructor }) => instructor.blog.userBlogs.drafts
+    })
+  },
   async fetch({ store }) {
     await store.dispatch('instructor/blog/fetchUserBlogs')
   }
