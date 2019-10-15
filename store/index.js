8527580a28052542156cbc0f588d960d2b0b8a27
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-15 14:28:27 +0300

    add hero component dynamic data

--- store/index.js
+++ store/index.js
@@ -2,8 +2,11 @@ export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
     try {
       await dispatch('auth/loadUSer')
+      await dispatch('hero/fetchHero').catch(() => console.log('Cannot fetch page data'))
     } catch (error) {
       console.error(error)
     }
   }
 }
+
+// initial fetch on server
