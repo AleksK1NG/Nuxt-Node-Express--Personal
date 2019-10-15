73ebd16427d3a166711be54b03ead4a6128d7892
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-15 16:21:52 +0300

    add course module fetchCourseBySlug action

--- store/course.js
+++ store/course.js
@@ -1,6 +1,7 @@
-import { SET_COURSES, SET_ERROR, SET_LOADING } from '~/store/constants'
+import { SET_COURSE, SET_COURSES, SET_ERROR, SET_LOADING } from '~/store/constants'
 
 export const state = () => ({
+  course: null,
   courses: [],
   isLoading: false,
   error: null
@@ -21,11 +22,28 @@ export const actions = {
       commit(SET_LOADING, false)
       return error
     }
+  },
+
+  async fetchCourseBySlug({ commit }, courseSlug) {
+    commit(SET_LOADING, true)
+    try {
+      const course = await this.$axios.$get(`/api/v1/products/s/${courseSlug}`)
+      commit(SET_COURSE, course)
+      commit(SET_LOADING, false)
+      commit(SET_ERROR, null)
+      return course
+    } catch (error) {
+      console.error(error)
+      commit(SET_ERROR, error)
+      commit(SET_LOADING, false)
+      return error
+    }
   }
 }
 
 export const mutations = {
   [SET_COURSES]: (state, courses) => (state.courses = courses),
+  [SET_COURSE]: (state, course) => (state.course = course),
   [SET_LOADING]: (state, loading) => (state.isLoading = loading),
   [SET_ERROR]: (state, error) => (state.error = error)
 }
