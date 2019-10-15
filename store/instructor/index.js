1d653dc6ed2c0caba7698a57c35aebcc14e16388
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-10-15 15:40:41 +0300

    add set home activeHero action

--- store/instructor/index.js
+++ store/instructor/index.js
@@ -1,4 +1,4 @@
-import { SET_ERROR, SET_HEROES, SET_LOADING } from '~/store/constants'
+import { SET_ERROR, SET_HERO, SET_HEROES, SET_LOADING } from '~/store/constants'
 
 export const state = () => ({
   heroes: [],
@@ -20,11 +20,29 @@ export const actions = {
 
     try {
       const heroes = await this.$axios.$get('/api/v1/product-heroes')
-      debugger
+
       commit(SET_HEROES, heroes)
       commit(SET_ERROR, null)
       commit(SET_LOADING, false)
       return heroes
+    } catch (error) {
+      console.error(error)
+      commit(SET_ERROR, error)
+      commit(SET_LOADING, false)
+      return error
+    }
+  },
+
+  async activateHero({ commit }, heroId) {
+    commit(SET_LOADING, true)
+
+    try {
+      const activeHero = await this.$axios.$patch(`/api/v1/product-heroes/${heroId}`)
+
+      commit(`hero/${SET_HERO}`, activeHero, { root: true })
+      commit(SET_ERROR, null)
+      commit(SET_LOADING, false)
+      return activeHero
     } catch (error) {
       console.error(error)
       commit(SET_ERROR, error)
